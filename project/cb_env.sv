171a7823f989afdba5d1250d37f89a03f789be3d
Sheethal Rosemary Thankachan <014547286@coe-ee-cad31.sjsuad.sjsu.edu>
2021-04-15 15:28:27 -0700

    Rev1.2

--- project/cb_env.sv
+++ project/cb_env.sv
@@ -1,7 +1,7 @@
 class cb_env extends uvm_env;
 `uvm_component_utils(cb_env)
 //cb_mon_out monout;
-//cb_scoreboard0 scbd0;
+cb_scoreboard0 scbd0;
 //cb_scoreboard1 scbd1;
 cb_agent agnt1;
 function new (string name="cb_env",uvm_component parent =null);
@@ -9,7 +9,7 @@ super.new(name,parent);// The super keyword is used from within a derived class 
 endfunction :new
 
 function void build_phase(uvm_phase phase);
-	//scbd0=cb_scoreboard0::type_id::create("scbd0",this);
+	scbd0=cb_scoreboard0::type_id::create("scbd0",this);
 	//scbd1=cb_scoreboard1::type_id::create("scbd1",this);
 	agnt1=cb_agent::type_id::create("agnt1",this);
 	//monout=cb_mon_out::type_id::create("monout",this);
@@ -17,7 +17,7 @@ function void build_phase(uvm_phase phase);
 endfunction: build_phase
 
 function void connect_phase(uvm_phase phase);
-	//agnt1.monin.pdat.connect(scbd0.message_in.analysis_export);
+	agnt1.monin.pdat.connect(scbd0.message_in.analysis_export);
 	//monout.pdat1.connect(scbd0.message_out);
 	//monout.psc1.connect(scbd1.empty_in.analysis_export);
 endfunction:connect_phase
--- project/cb_scoreboard0.sv
+++ project/cb_scoreboard0.sv
@@ -7,40 +7,36 @@ class cb_scoreboard0 extends uvm_scoreboard;
 `uvm_component_utils(cb_scoreboard0)
 uvm_tlm_analysis_fifo #(mimsg) message_in;
 //uvm_tlm_analysis_fifo #(logic) message_out;
-uvm_blocking_put_imp #(logic,cb_scoreboard0) message_out;
+//uvm_blocking_put_imp #(logic,cb_scoreboard0) message_out;
 
 mimsg m;
-int count;
-logic sh_full;
+//int count;
+//logic sh_full;
 function new (string name="cb_scoreboard0",uvm_component parent=null);
 	super.new(name,parent);
 endfunction: new
 
 function void build_phase(uvm_phase phase);
 	message_in=new("message_in",this);
-	message_out=new("message_out",this);
+	//message_out=new("message_out",this);
 endfunction:build_phase
-task put(logic sh_full);
+/*task put(logic sh_full);
 		//$display("sh_full=%0d",sh_full);		
 			if(count >=4) begin
 				if(sh_full!=1) begin
 				`uvm_fatal("FIFOfull","FIFO should be full but full is not set")
 				end
 			end
-endtask
+endtask*/
 task run_phase(uvm_phase phase);
 	forever begin
 		message_in.get(m);
 		//$display("pushin of m",m.pushin);
-		if(m.pushin && m.write && !m.full) begin
-			count++;
-			$display("count++ UVM=%0d",count);		
+		if((m.pushin)) begin
+			$display("Data from Scoreboard0 UVM =%h",m.datain);		
 		end
-		else begin
-			if(!m.pushin && !m.write) begin
-			count--;
-			end
-		end
+		//else
+		//$display("I dint get anyting");
 	end
 endtask:run_phase
 
